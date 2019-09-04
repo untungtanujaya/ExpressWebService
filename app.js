5f4e55e2d86f622ab2a636c889b1505ed1b4c36d
Juan <juanfelixparsaoran@gmail.com>
2019-09-04 11:21:15 +0700

    send file in request

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/LogAct_Minggu1_Juan.pdf'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
